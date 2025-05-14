3f375f2224b7c7c634fcbf4fa37798765d4ecf47
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-05-15 00:47:53 +0100

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 const App = () => {
 	return (
 		<div className="app ">
 			<Header />
+			<main className="main"></main>
+			<Footer />
 		</div>
 	);
 };
